f16bdea424ce7506a5999595e3ba69fdbcac7b51
khepri_kreator <makareev.dima@yandex.ru>
2024-05-31 23:45:18 +0800

    fix numberCells array observation

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,24 +2,26 @@ import {makeObservable, observable, action} from 'mobx';
 import {NumberCell} from '../models'
 
 export default class GameStore {
+    difficult: 21|56|70 = 21;
     index: number = 0;
-    numbers: number[] = Array(7).fill(1).map((_, i) => i + 1)
+    numbers: number[] = Array(this.difficult).fill(1).map((_, i) => i + 1)
     numberCells: NumberCell[] = [];
     constructor() {
         makeObservable({
-            numberCells: observable,
+            numberCells: observable.array,
             compareValues: action,
         })
     }
     shuffleNumbersAndFillNumberCells() {
         const numbersCopy = this.numbers.map((n) => n);
         for (let i = numbersCopy.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
+            const j = Math.floor(Math.random() * (i + 1));
             [numbersCopy[i], numbersCopy[j]] = [numbersCopy[j], numbersCopy[i]]
         }
         this.numberCells = numbersCopy.map((n): NumberCell => {
             return {value: n, isPressed: false}
         })
+        this.numberCells = observable.array(this.numberCells)
         console.log(this.numbers, this.numberCells)
     }
     compareValues(value: number) {
